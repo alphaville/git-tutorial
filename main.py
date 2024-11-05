67c6650d8307c984d621ea03a37f49487194a20b
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-11-05 13:02:42 +0000

    feat: implement sum of cubes

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ def sum_squares(x):
     return s
 
 
+def sum_cubes(x):
+    s = 0
+    for xi in x:
+        s += xi**3
+    return s
+
+
 z = [1, 2, 0, 3]
 r = sum_squares(z)
 assert r == 14
+
+
+a = [1, 2, 3]
+expected_result = 36
+sc = sum_cubes(a)
+print(sc)
+assert expected_result == sc
